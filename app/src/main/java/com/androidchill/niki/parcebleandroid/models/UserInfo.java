3266aaf7cfdbc3d8e9cf284fa57bc79a5b7c588e
NIKI <nikiizvorski@gmail.com>
2016-10-15 10:20:52 +0100

    Added UserInfo Parcelable

--- app/src/main/java/com/androidchill/niki/parcebleandroid/models/UserInfo.java
+++ app/src/main/java/com/androidchill/niki/parcebleandroid/models/UserInfo.java
@@ -129,12 +129,7 @@ public class UserInfo implements Parcelable {
             return 0;
         }
 
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            dest.writeString(street);
-            dest.writeString(city);
-            dest.writeInt(number);
-        }
+
 
         // Creator
         public static final Parcelable.Creator<Address> CREATOR
